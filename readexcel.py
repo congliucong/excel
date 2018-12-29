8a8dae9f4ab7a039618acdcafd7f934e2d9c630e
liucongcong1 <congcong.liu@hotmail.com>
2018-12-29 18:00:30 +0800

    python crawl

--- readexcel.py
+++ readexcel.py
@@ -41,30 +41,42 @@ def open_excel(file_name, file_path):
     for index in range(0, sheet.nrows):
         if index == 0:
             continue
+        print('-- '+str(index))
         row_data = sheet.row_values(index)
-        fromname = row_data[0]
-        fromphone = row_data[1]
-        fromaddress = row_data[2]
-        rename = row_data[3]
-        rephone = row_data[4]
+        id = row_data[0]
+        submittime = row_data[1]
+        orderno = row_data[2]
+        fromname = row_data[3]
+        receivename = row_data[4]
         recom = row_data[5]
-        readdress = row_data[6]
-        towhere = row_data[7]
-        what = row_data[8]
+        city = row_data[6]
+        provice = row_data[7]
+        summary = row_data[8]
         protect = row_data[9]
         protectprice = row_data[10]
-        submittime = row_data[11]
-        orderno = row_data[12]
-        fromno = row_data[13]
-        price = row_data[14]
-        weight = row_data[15]
-        type = row_data[16]
-        provice = row_data[17]
-        dataarr = ['' + fromname + '', fromphone, '' + fromaddress + '', '' + rename + '', '' + rephone + '', '' + recom + '', '' + readdress + '',
-                   '' + towhere + '','' + what + '', '' + protect + '', '' + protectprice + '', '' + submittime + '', '' + orderno + '', '' + fromno + '',
-                   '' + price + '', weight, '' + type + '', '' + provice + '']
-        sql = 'insert into list(fromname,fromphone,fromaddress,rename,rephone,recom,readdress,towhere,what,protect,protectprice,submittime,orderno,fromno,price,weight,type,provice)' \
-              ' values ("'+ fromname + '", "' + fromphone + '", "' + fromaddress + '", "' + rename + '", "' + rephone + '", "' + recom + '", "' + readdress + '", "' + towhere + '", "' + what + '", "' + protect + '", "' + protectprice + '", "' + submittime + '", "' + orderno + '", "' + fromno + '", "' + price + '", "' + weight + '", "' + type + '", "' + provice + '")'
+        protectmoney = row_data[11]
+        weight = row_data[12]
+        expressmoney = row_data[13]
+        toems = row_data[14]
+        totalmoney = row_data[15]
+        budetdep = row_data[16]
+        signinfo = row_data[17]
+        project = row_data[18]
+        firstweight = row_data[19]
+        sencodweight = row_data[20]
+        firstweightprice = row_data[21]
+        sencondweightprice = row_data[22]
+        protectprice2 = row_data[23]
+        price2 = row_data[24]
+        diff = row_data[25]
+        # dataarr = ['' + fromname + '', fromphone, '' + fromaddress + '', '' + rename + '', '' + rephone + '', '' + recom + '', '' + readdress + '',
+        #            '' + towhere + '','' + what + '', '' + protect + '', '' + protectprice + '', '' + submittime + '', '' + orderno + '', '' + fromno + '',
+        #            '' + price + '', weight, '' + type + '', '' + provice + '']
+        sql = 'insert into list(id, submittime, orderno, fromname, receivename, recom, city, provice, summary, protect, protectprice, protectmoney, weight, expressmoney, toems, ' \
+              'totalmoney, budetdep, signinfo, project, firstweight, sencodweight, firstweightprice, sencondweightprice, protectprice2, price2, diff)' \
+              ' values ("'+ id + '", "' + submittime + '", "' + submittime + '", "' + orderno + '", "' + fromname + '", "' + receivename + '", "' + recom + '", "' + city + '", "' + provice + '", "' + summary + '", "' + protect + '", "' + protectprice + '", "' + protectmoney + '", "' + weight + '", "' + expressmoney + '", "' + toems + '", ' \
+                    '"' + totalmoney + '", "' + budetdep + '", "' + signinfo + '", "' + project + '", "' + firstweight + '", "' + sencodweight + '", ' \
+                    '"' + firstweightprice + '", "' + sencondweightprice + '", "' + protectprice2 + '", "' + price2 + '", "' + diff + '");'
         print(sql)
         # sqlArr.append(dataarr)
         # else:
@@ -83,6 +95,6 @@ def open_excel(file_name, file_path):
 
 
 if __name__ == '__main__':
-    path = 'D:\\ems'
+    path = 'D:\\ems1'
     read_excel(path)
 
